61e9e7aa4e65b9d3d4f44e156d1556aaf170a6bc
Joel Rodrigues Moreira <joel.databox@gmail.com>
2014-12-07 12:27:20 -0200

    Adicionado get and set

--- Projet/SmartGit/src/br/com/maxx/bean/Pessoa.java
+++ Projet/SmartGit/src/br/com/maxx/bean/Pessoa.java
@@ -5,9 +5,54 @@ package br.com.maxx.bean;
  * @author Joel Rodrigues
  */
 public class Pessoa {
+
     private String cidade;
     private Long id;
     private String nome;
     private Integer idade;
     private String cpf;
+
+    public Pessoa() {
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
 }

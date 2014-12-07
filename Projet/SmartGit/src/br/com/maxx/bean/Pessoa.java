b6f941291189f3aae0c86cd60edce79de9a73e2b
Joel Rodrigues Moreira <joel.databox@gmail.com>
2014-12-07 11:55:35 -0200

    Adicionado cidade

--- Projet/SmartGit/src/br/com/maxx/bean/Pessoa.java
+++ Projet/SmartGit/src/br/com/maxx/bean/Pessoa.java
@@ -5,6 +5,7 @@ package br.com.maxx.bean;
  * @author Joel Rodrigues
  */
 public class Pessoa {
+    private String cidade;
     private Long id;
     private String nome;
     private Integer idade;
